a011cc5c0f1dac5cf61f74ec8d94ecef76de2754
Yohanes <yohanesmulugeta21@gmail.com>
2022-11-09 22:46:24 +0300

    handle the get request to /api/v1/tours

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const app = require('express')();
+const fs = require('fs');
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', authod: 'Yohanes' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', authod: 'Yohanes' });
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post to this endpoint...');
+// });
+
+// Top level READING
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// the ROUTE handler is the call back fun that handle the req
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
 });
 
 const port = 3000;
