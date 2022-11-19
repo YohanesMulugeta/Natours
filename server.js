5ea4aed9e2232f1dc065c52c6d6041c53f76790e
Yohanes <yohanesmulugeta21@gmail.com>
2022-11-19 09:19:11 +0300

    install mongoose and connect our remote databse to our express app

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+(async () => {
+  const con = await mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
+  console.log('Database connection is successful!');
+})();
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() =>
+//     console.log('Database connection is successful!')
+//   );
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
