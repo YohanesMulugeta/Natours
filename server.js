1846280e00480159d0640e99e923beb788009942
Yohanes <yohanesmulugeta21@gmail.com>
2022-11-19 10:34:21 +0300

    create schema for tour and create Tour model out of it

--- server.js
+++ server.js
@@ -20,16 +20,21 @@ const DB = process.env.DATABASE.replace(
   console.log('Database connection is successful!');
 })();
 
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() =>
-//     console.log('Database connection is successful!')
-//   );
+// Modle is like a blue print to create documents
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a price'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
