12707840e81be6cb78edc775f15eb1d81e9a3e14
Yohanes <yohanesmulugeta21@gmail.com>
2022-12-30 22:45:28 +0300

    send errors based on the environment we are working currently that is if we are development invironment we would like to see everything and in produvtion we want to send generic messages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,42 @@
+function sendErrorDev(err, res) {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+function sendErrorPro(err, res) {
+  // Trusted operational errors
+  if (err.isOperatiional) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // UNKOWN errors so that we dont want to send errors to the client
+
+    // 1) log the error
+    console.error('ERROR', err);
+
+    // 2) send generic responses
+    res.status(500).json({
+      status: 'fail',
+      message: 'Something went out bery wrong, please try again',
+    });
+  }
+}
+
 module.exports = function (err, req, res, next) {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    sendErrorPro(err, res);
+  }
 };
