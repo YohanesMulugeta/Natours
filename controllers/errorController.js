83c545e3229cf77ff7d3b006f4f07189a8cd4826
Yohanes <yohanesmulugeta21@gmail.com>
2023-01-06 15:58:32 +0300

    handle validation error and Duplicate key error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,26 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+const AppError = require('../utils/appError');
+
+function handleCastErrorDB(err) {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+}
+
+function handleDuplicateFields(err) {
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value} please use another value`;
+  return new AppError(message, 400);
+}
+
+function handleValidationError(err) {
+  const message = Object.values(err.errors)
+    .map((el) => el.message)
+    .join('. ');
+
+  return new AppError(message, 400);
+}
+
 function sendErrorDev(err, res) {
   res.status(err.statusCode).json({
     status: err.status,
@@ -18,12 +41,12 @@ function sendErrorPro(err, res) {
     // UNKOWN errors so that we dont want to send errors to the client
 
     // 1) log the error
-    console.error('ERROR', err);
+    // console.error('ERROR', err);
 
     // 2) send generic responses
     res.status(500).json({
       status: 'fail',
-      message: 'Something went out bery wrong, please try again',
+      message: 'Something went out very wrong, please try again',
     });
   }
 }
@@ -31,12 +54,18 @@ function sendErrorPro(err, res) {
 module.exports = function (err, req, res, next) {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Error';
+  let error = { ...err };
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   }
+  // console.log(err.name);
+  if (err.code === 11000) error = handleDuplicateFields(err);
+  if (err.name === 'CastError') error = handleCastErrorDB(error);
+  if (err.name === 'ValidationError')
+    error = handleValidationError(err);
 
   if (process.env.NODE_ENV === 'production') {
-    sendErrorPro(err, res);
+    sendErrorPro(error, res);
   }
 };
