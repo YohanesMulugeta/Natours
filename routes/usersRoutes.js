e8662dc9920e505032de962422d7b9d42dba65d2
Yohanes <yohanesmulugeta21@gmail.com>
2023-02-01 15:26:28 +0300

    create a controller for login route. create an instance method called isCorrect to check the correctness of a password

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -12,6 +12,7 @@ const authController = require('../controllers/authController');
 // 1) CREATING THE ROUTER
 const router = express.Router();
 router.post('/signup', authController.signUp);
+router.post('/login', authController.login);
 
 // 1) HANDLING ROUTES
 router.route('/').get(getAllUsers).post(createUser);
