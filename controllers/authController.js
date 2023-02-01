502444bcd496d533db43fd32fb855ec66b9be698
Yohanes <yohanesmulugeta21@gmail.com>
2023-02-01 13:11:08 +0300

    log the user in on signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,12 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
+
+const sign = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXP,
+  });
 
 exports.signUp = catchAsync(async (req, res, next) => {
   // 1) recieve onlly the fields we want to store to our user document
@@ -8,9 +15,12 @@ exports.signUp = catchAsync(async (req, res, next) => {
   // 2) create user
   const newUser = await User.create({ name, email, password, passwordConfirm });
 
-  // 3) send response
+  // 3) signIn user
+  const token = sign(newUser._id);
+
+  // 3) send response with the token
   res.status(201).json({
     message: 'success',
-    data: { newUser },
+    token,
   });
 });
