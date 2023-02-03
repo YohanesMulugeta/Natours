0f140c6fe69b92e5171d066e93df0a906ef11838
Yohanes <yohanesmulugeta21@gmail.com>
2023-02-03 17:16:00 +0300

    code forgot password auth handler to implement forgot password. And code a user instance method called createPasswordResetToken to create a resetToken and store the encrypted one on the database

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -7,6 +7,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
 
 // 1) HANDLING ROUTES
 router
