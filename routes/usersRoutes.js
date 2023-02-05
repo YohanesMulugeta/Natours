eb4b894b34d5b381b57be74f949fa91e0fa5d4ff
Yohanes <yohanesmulugeta21@gmail.com>
2023-02-05 06:38:02 +0300

    code passwordReset function to handle password reset. also update the pre save userSchema middleware to work with password reset.

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:resetToken', authController.resetPassword);
 
 // 1) HANDLING ROUTES
 router
