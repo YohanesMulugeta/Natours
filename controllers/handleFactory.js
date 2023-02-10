a295780dd248b3d06847317ea53c1dbd9ca64774
Yohanes <yohanesmulugeta21@gmail.com>
2023-02-10 16:39:06 +0300

    code getAll and getOne on the handleFactory to handle reading of oneFile and All files

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async function (req, res, next) {
@@ -45,6 +46,50 @@ exports.createOne = (Model) =>
       status: 'success',
       data: {
         data: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, populateOptions) =>
+  catchAsync(async function (req, res, next) {
+    const query = Model.findById(req.params.id);
+
+    if (populateOptions) query.populate(populateOptions);
+
+    const doc = await query;
+
+    if (!doc)
+      return next(
+        new AppError(`No document with the Id: ${req.params.id}.`, 404)
+      );
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async function (req, res, next) {
+    // To allow for nested route reviews on a tour
+    const filter = {};
+    if (req.params.tourId) filter.tour = req.params.tourId;
+
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .project()
+      .paginate();
+
+    const docs = await features.mongooseQueryObject;
+
+    res.status(200).json({
+      status: 'success',
+      result: docs.length,
+      data: {
+        data: docs,
       },
     });
   });
