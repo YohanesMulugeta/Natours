5a08e145355c1faa1ec92d31c0647022288093ab
Yohanes <yohanesmulugeta21@gmail.com>
2023-02-14 15:47:38 +0300

    biuld the overview template and with the data from the database.

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,13 +1,15 @@
+const catchAsync = require('../utils/catchAsync');
 const Tour = require('../model/tourModel');
 
-exports.getOverview = (req, res, next) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) get All the tour deta from our collection
+  const tours = await Tour.find();
 
   //   2) build template
 
   //   3) Render the template using the tour data from 1)
-  res.status(200).render('overview', { title: 'All Tours' });
-};
+  res.status(200).render('overview', { title: 'All Tours', tours });
+});
 
 exports.getTourDetail = (req, res, next) => {
   res.status(200).render('tour', { title: 'The Sea Explorer' });
