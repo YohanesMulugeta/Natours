836ed2f6b14afe36c2bab6bef7088c562f89cfbb
Yohanes <yohanesmulugeta21@gmail.com>
2023-02-16 16:31:43 +0300

    make mapbox work with the locations of a tour

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,10 +1,49 @@
 /*eslint-disable */
+const locations = JSON.parse(document.querySelector('#map').dataset.locations);
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoieW9oYW5lczIxIiwiYSI6ImNsZTYyemh1djBmbWszdnM0ZHdwYm02czYifQ._NwU-4qyQ2dl9DVZq0GvvQ';
+
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+  style: 'mapbox://styles/yohanes21/cle72qxev004601q9fdmbazeo/draft', // style URL
+  scrollZoom: false,
+  // center: [-80.185942, 25.774772], // starting position [lng, lat]
+  // zoom: 2, // starting zoom
+  // interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create Marker
+  const el = document.createElement('div');
+  el.classList.add('marker');
+
+  // Add Marker
+  const marker = new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setPopup(
+      // Create Popup
+      new mapboxgl.Popup({ offset: 30 })
+        .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+        .addTo(map)
+    )
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  marker.togglePopup();
+  // Include the current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 150,
+    bottom: 100,
+    left: 100,
+    right: 100,
+  },
 });
